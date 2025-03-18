be340ad90f1d90fb36a131199156c1dbd735770a
strangelifekid <134167125+strangelifekid@users.noreply.github.com>
2025-03-18 17:52:43 +0100

    änderungen website

---
+++ components.js
@@ -0,0 +1,21 @@
+const header = `<header>
+<nav>
+<ul>
+<li><a href="./index.html#top">Home</a></li>
+                <div class="grow"></div>
+                <li><a href="./index.html#aboutMe">About Me</a></li>
+                <li><a href="./index.html#projects">Projects</a></li>
+                <li><a href="./index.html#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    `
+
+class headerClass extends HTMLElement {
+    constructor () {
+        super();
+        this.innerHTML=header;
+
+    }
+}
+customElements.define("header-class",headerClass);
